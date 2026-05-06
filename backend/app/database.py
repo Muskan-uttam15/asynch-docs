32c40ea306984d41808da5ee624542c5ba16c9bd
Muskan uttam <muskanuttam867@gmail.com>
2026-05-06 17:09:23 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(settings.database_url, future=True)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 
 class Base(DeclarativeBase):
